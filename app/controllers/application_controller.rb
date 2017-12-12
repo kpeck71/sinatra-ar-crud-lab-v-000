d1b7cfe9b479185b5ed0fb5cb7b0bb2157d3fa5e
Kristen Peck <kpeck71@gmail.com>
2017-12-12 03:52:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,6 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do
-    binding.pry
    @post = Post.find(params[:id])
    @post.name = params[:name]
    @post.content = params[:content]
